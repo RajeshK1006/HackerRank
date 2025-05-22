04f010a89ee88efa0bb89915d09263111388d83e
Rajesh <143991442+RajeshK1006@users.noreply.github.com>
2025-05-22 12:21:13 +0530

    Create HashSet_problem.java

---
+++ HashSet_problem.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class HashSet_problem {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc  = new Scanner(System.in);
+        HashSet<String> s = new HashSet<>();
+        int n = sc.nextInt();
+        sc.nextLine();
+        for(int i=0;i<n;i++){
+            String[] arr = sc.nextLine().split(" ");
+            Arrays.sort(arr);
+            
+            
+            String res = arr[0] + "," + arr[1];
+            s.add(res);
+            System.out.println(s.size());
+            
+        }
+        
+        
+    }
+}
